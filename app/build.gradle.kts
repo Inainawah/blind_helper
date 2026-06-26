ec917a54612f101c4ea0137ca980cb8279b062e5
dayday <danny931228@gmail.com>
2026-06-26 15:43:08 +0800

    畫面正方形

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,6 +94,8 @@ dependencies {
 
   //任務四優化：補上手機端 GPU 加速與神經網路 API 支持
   implementation("org.tensorflow:tensorflow-lite-gpu:2.16.1")
+  implementation("org.tensorflow:tensorflow-lite:2.14.0")
+  implementation("org.tensorflow:tensorflow-lite-support:0.4.4")
 }
 
 
